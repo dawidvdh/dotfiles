10bfdde887abbcfe2c4a29c685d43964affb8b5a
Dawid van der Hoven <dawidvdh@gmail.com>
2015-07-11 14:28:44 +0200

    install android sdk instead of juts the tools

--- brew.sh
+++ brew.sh
@@ -100,7 +100,7 @@ brew install tree
 # brew install webkit2png
 # brew install zopfli
 brew install node # This installs `npm` too using the recommended installation method
-brew install android-platform-tools
+brew install android-sdk
 brew install zsh
 # Remove outdated versions from the cellar.
 brew cleanup
