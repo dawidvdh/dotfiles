3c6989451f94bdbb66e38f8d3c0333e725cc2a6d
dawidvdh <dawidvdh@gmail.com>
2020-02-28 11:42:42 +0100

    updates

--- brew-cask.sh
+++ brew-cask.sh
@@ -4,7 +4,6 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 # Daily
 brew cask install 1password
-brew cask install adobe-creative-cloud
 
 # Dev
 brew cask install visual-studio-code-insiders
@@ -12,27 +11,20 @@ brew cask install iterm2
 brew cask install imageoptim
 brew cask install macdown
 brew cask install xquartz
-Brew cash install robo-3t
-Brew cask install android-studio
+brew cask install android-studio
+brew cask install insomnia
+brew cask install react-native-debugger
 
 # browsers
 brew cask install google-chrome-canary
 brew cask install google-chrome
-brew cask install firefox
-brew cask install firefoxnightly
-brew cask install webkit-nightly
-brew cask install safari-technology-preview
-brew cask install chromium
-brew cask install opera
 
 # less often
-brew cask install vlc
 brew cask install transmission
 brew cask install dropbox
-brew cask install gpgtools
 brew cask install spectacle
-brew cask install whatsapp
 brew cask install licecap
+brew cask install spotify
 
 echo "Running brew cask caveats."
 
